2dd01ac6cf36b2ca8288f0a29f058b5935f46e3d
Rato <rato.aq2@gmail.com>
2016-10-15 14:54:09 +0200

    Fixing travis build

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def find_version(*file_paths):
 setup_requirements = ['pytest-runner'] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else []
 install_requirements = ['enzyme>=0.4.1', 'pymediainfo>=2.1.5', 'six>=1.9.0']
 test_requirements = ['flake8_docstrings', 'flake8-import-order', 'pydocstyle!=1.1.0', 'pep8-naming',
-                     'pytest', 'pytest-flake8']
+                     'pytest>=2.8', 'pytest-flake8']
 
 setup(
     name='knowit',
